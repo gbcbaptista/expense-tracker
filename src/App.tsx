4d02377bb6e7c14b4cdc8365c006dd46e2e83abc
Gabriel Baptista Cerqueira <66979159+gbcbaptista@users.noreply.github.com>
2022-08-09 20:11:57 -0300

    categories and filter function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import * as C from './App.styles';
+import { Category } from './types/category';
+import { Item } from './types/item';
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { useEffect, useState } from 'react';
+import { getCurrentMonth } from './helpers/dateFilter';
+
 
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(
+    () => {
+
+    }, [list, currentMonth]
+  );
+  
+
   return (
     <C.Container>
       <C.Header>
