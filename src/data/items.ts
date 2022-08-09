94bd42360ff61dd75381d1ba85aee5ea101f62b1
Gabriel Baptista Cerqueira <66979159+gbcbaptista@users.noreply.github.com>
2022-08-09 19:38:53 -0300

    starting project initial changes

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/item';
+
+export const items: Item[] = [
+    { date: new Date(2021, 9, 15), category: 'food', title: 'Burguer King', value: 32.12 },
+    { date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 28 },
+    { date: new Date(2021, 9, 16), category: 'rent', title: 'Burguer King', value: 2300.10 },
+    { date: new Date(2021, 9, 18), category: 'salary', title: 'Salary ACME', value: 13200 },
+];
