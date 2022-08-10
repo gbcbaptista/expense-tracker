f26db6ce5c2c28449e4ec88819aad6d22a16c22c
Gabriel Baptista Cerqueira <66979159+gbcbaptista@users.noreply.github.com>
2022-08-09 21:15:32 -0300

    format dates

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,3 +1,4 @@
+import { formatDate } from '../../helpers/dateFilter';
 import { Item } from '../../types/Item';
 import * as C from './styles';
 
@@ -8,7 +9,7 @@ type Props = {
 export const TableItem = ({ item }: Props) => {
     return (
         <C.TableLine>
-            <C.TableColumn>Data</C.TableColumn>
+            <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
             <C.TableColumn>{item.category}</C.TableColumn>
             <C.TableColumn>{item.title}</C.TableColumn>
             <C.TableColumn>R$ {item.value}</C.TableColumn>
