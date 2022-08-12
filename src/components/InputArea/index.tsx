ce76c5b9cc7a698848280c8da57fa9d9311822e9
Gabriel Baptista Cerqueira <66979159+gbcbaptista@users.noreply.github.com>
2022-08-12 00:48:17 -0300

    implementation for adding a expense or income

--- src/components/InputArea/index.tsx
+++ src/components/InputArea/index.tsx
@@ -1,10 +1,15 @@
 
+import { categories } from '../../data/categories';
+import { formatDate } from '../../helpers/dateFilter';
+import { Category } from '../../types/Category';
 import { Item } from '../../types/Item';
 import * as C from './styles';
 
 type Props = {
     onAdd: (item: Item) => void;
 }
+
+
 
 export const InputArea = ({onAdd}: Props) => {
 
@@ -20,7 +25,46 @@ export const InputArea = ({onAdd}: Props) => {
 
     return (
         <C.Container>
+            <div>
+                <div>
+                    <label>Date</label>
+                    <input
+                        id="itemDate"
+                        name="itemDate"
+                        type="date"
+                    />
+                </div>
+                <div>
+                    <label>Category</label>
+                    <input
+                        id="itemDate"
+                        name="itemDate"
+                        type="text"
+                    />
+                </div>
+                <div>
+                    <label>Title</label>
+                    <input
+                        id="itemDate"
+                        name="itemDate"
+                        type="text"
+                    />
+                </div>
+                <div>
+                    <label>Value</label>
+                    <input
+                        id="itemDate"
+                        name="itemDate"
+                        type="number"
+                    />
+                </div>
+            </div>
             <button onClick={handleAddEvent}>Add Item</button>
+            
+            
+
+            
+            
         </C.Container>
     );
 }
--- src/components/InputArea/styles.ts
+++ src/components/InputArea/styles.ts
@@ -2,9 +2,34 @@ import styled from "styled-components";
 
 
 export const Container = styled.div`
+    
+
     background-color: #FFF;
     box-shadow: 0px 0px 5px #CCC;
     border-radius: 10px;
     padding:20px;
     margin-top: 20px;
+
+    div {
+        display: grid;
+        grid-template-columns: auto;
+        text-align:center;
+        grid-row: 1;
+    }
+
+    label {
+        margin: 5px;
+    }
+
+    input, select {
+        background-color: #FFF;
+        padding: 10px;
+        border: 1px solid #CCC;
+        border-radius: 5px;
+        margin-right: 10px;
+    }
+
+    button {
+        
+    }
 `;
